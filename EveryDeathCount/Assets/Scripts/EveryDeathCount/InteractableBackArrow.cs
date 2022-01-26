0ac6d352d26629990d6a8910bd1b1f63401b2617
Maureen-gdd <maureen.grandidier@gmail.com>
2022-01-26 12:16:37 +0100

    Scene Lecter Completed

--- EveryDeathCount/Assets/Scripts/EveryDeathCount/InteractableBackArrow.cs
+++ EveryDeathCount/Assets/Scripts/EveryDeathCount/InteractableBackArrow.cs
@@ -7,9 +7,12 @@ public class InteractableBackArrow : MonoBehaviour, IInteractable
 {
     public GameObject choiceCanvas;
     public GameObject[] interactablesObjects;
+    public AudioSource musicBackground;
 
     public void OnClickAction(InputAction.CallbackContext context)
     {
+        musicBackground.Play();
+        
         choiceCanvas.SetActive(false);
 
         foreach (var interactable in interactablesObjects)
